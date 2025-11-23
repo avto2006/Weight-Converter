4567f903ce35c6c5e1e0092c8b8fa30a3933732a
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-11-23 16:02:47 +0400

    some

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let lbs = inp.value
 const calculate = () => {
     let lbs = parseFloat(inp.value) || 0
     let kg =  lbs * 0.453592
-     inner.innerHTML= `Your weight in kg is: ${kg}`
+     inner.innerHTML= `Your weight in kg is: ${kg.toFixed(2)}`
 }
 
 inp.addEventListener('input', calculate)
